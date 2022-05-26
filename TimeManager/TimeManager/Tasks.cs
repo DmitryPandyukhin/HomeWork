a284fb8b568926612ef73d8aecff9d6c3993cc09
pandyukhin <pandyukhin@gmail.com>
2022-05-26 04:12:39 +0300

    Создан макет формы

---
+++ TimeManager/TimeManager/Tasks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManager
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        // Время в минутах
+        public int Time { get; set; }
+        public Task()
+        {
+            Title = "";
+        }
+    }
+}
