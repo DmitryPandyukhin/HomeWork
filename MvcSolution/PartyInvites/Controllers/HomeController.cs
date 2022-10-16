a124655933d8cb5a08eab8a4e608e43b6e415d46
DmitryPandyukhin <pandyukhin@gmail.com>
2022-10-16 14:06:55 +0300

    Добавлена ссылка на представление анкеты. Добавлен вызов формы анкеты из контроллера

--- MvcSolution/PartyInvites/Controllers/HomeController.cs
+++ MvcSolution/PartyInvites/Controllers/HomeController.cs
@@ -16,5 +16,9 @@ namespace PartyInvites.Controllers
             ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
             return View("MyView");
         }
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
     }
 }
