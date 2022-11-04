7a35969b3a25c73415cb68d8d4765ff047a0e3bb
DmitryPandyukhin <pandyukhin@gmail.com>
2022-11-05 02:51:19 +0300

    Добавлен макет главной формы. Подключены фреймворки.
    Изменена главная форма.
    Добавлена форма поставщиков.
    Подключен сервис контекста БД.
    Получен из БД и выведен на форму список поставщиков

---
+++ ASPNETCore6/NorthwindWeb/Pages/suppliers.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+using Packt.Shared;
+
+namespace NorthwindWeb.Pages
+{
+    public class SuppliersModel : PageModel
+    {
+        public IEnumerable<string> Suppliers { get; set; }
+        private Northwind db;
+        public SuppliersModel(Northwind injectedContext)
+        {
+            db = injectedContext;
+        }
+        public void OnGet()
+        {
+            ViewData["Title"] = "Northwind Web Site - Поставщики";
+
+            /*Suppliers = new[] {
+                "Alpha Co", "Beta Limited", "Gamma Corp"
+            };*/
+            Suppliers = db.Suppliers.Select(s => s.CompanyName);
+        }
+    }
+}
